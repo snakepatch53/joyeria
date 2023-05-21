6c5de50940a29e30035454198331116e7d206fa6
snakepatch53 <snakepatch53@gmail.com>
2023-05-21 16:11:53 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   index.php
    	new file:   public/css.general/theme.css
    	deleted:    public/css.public/citas.css
    	deleted:    public/css.public/contactos.css
    	modified:   public/css.public/footer.css
    	modified:   public/css.public/header.css
    	modified:   public/css.public/inicio.css
    	deleted:    public/css.public/nosotros.css
    	modified:   src/assets/db.sql
    	modified:   src/routes/panel.php
    	modified:   src/routes/public.php
    	modified:   src/routes/services.php
    	modified:   src/templates/public.component/footer.php
    	modified:   src/templates/public.component/header.php
    	modified:   src/templates/public.pages/home.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 //show errors
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ob_start();
 session_start();
 // definimos la zona horaria
@@ -20,24 +20,10 @@ require_once(__DIR__ . '/src/functions/middlewares.php');
 // cargamos los objetos de acceso a datos
 require_once('./src/dao/InfoDao.php');
 require_once('./src/dao/UserDao.php');
-require_once('./src/dao/ClienteDao.php');
-require_once('./src/dao/ServicioDao.php');
-require_once('./src/dao/HoraDao.php');
-require_once('./src/dao/SliderDao.php');
-require_once('./src/dao/SocialDao.php');
-require_once('./src/dao/MensajeDao.php');
-require_once('./src/dao/CitaDao.php');
 
 // cargamos los servicios para el web service (WEB SERVICE)
 require_once('./src/services/info.service.php');
 require_once('./src/services/user.service.php');
-require_once('./src/services/cliente.service.php');
-require_once('./src/services/servicio.service.php');
-require_once('./src/services/hora.service.php');
-require_once('./src/services/slider.service.php');
-require_once('./src/services/social.service.php');
-require_once('./src/services/mensaje.service.php');
-require_once('./src/services/cita.service.php');
 
 // cargamos las variables de entorno
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
--- src/routes/panel.php
+++ src/routes/panel.php
@@ -7,14 +7,6 @@ $radapter->getHTML('/panel/login', 'login', fn () => middlewareSessionLogout());
 
 $radapter->getHTML('/panel', 'home', fn () => middlewareSessionLogin(), function ($DATA) {
     return [
-        'clientes_total' => count((new ClienteDao($DATA['mysqlAdapter']))->select()),
-        'horas_total' => count((new HoraDao($DATA['mysqlAdapter']))->select()),
-        'servicios_total' => count((new ServicioDao($DATA['mysqlAdapter']))->select()),
-        'citas_total' => count((new CitaDao($DATA['mysqlAdapter']))->select()),
-        'user_total' => count((new UserDao($DATA['mysqlAdapter']))->select()),
-        'social_total' => count((new SocialDao($DATA['mysqlAdapter']))->select()),
-        'slider_total' => count((new SliderDao($DATA['mysqlAdapter']))->select()),
-        'mensajes_total' => count((new MensajeDao($DATA['mysqlAdapter']))->select()),
         'info' => (new InfoDao($DATA['mysqlAdapter']))->select(),
     ];
 });
@@ -78,10 +70,6 @@ $radapter->getHTML('/panel/mensajes', 'mensajes', fn () => middlewareSessionLogi
 // CITAS
 $radapter->getHTML('/panel/citas', 'citas', fn () => middlewareSessionLogin(), function ($DATA) {
     return [
-        'info' => (new InfoDao($DATA['mysqlAdapter']))->select(),
-        'horas' => (new HoraDao($DATA['mysqlAdapter']))->select(),
-        'servicios' => (new ServicioDao($DATA['mysqlAdapter']))->select(),
-        'clientes' => (new ClienteDao($DATA['mysqlAdapter']))->select(),
-        'doctores' => (new UserDao($DATA['mysqlAdapter']))->selectDoctores(),
+        'info' => (new InfoDao($DATA['mysqlAdapter']))->select()
     ];
 });
